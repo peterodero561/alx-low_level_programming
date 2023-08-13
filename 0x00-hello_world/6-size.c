fe4ca1f9b013cfb4b88c598b518f30b1f757580d
Peter Odero <peterodero561@gmail.com>
2023-08-13 12:22:59 +0000

    Getting sizes of data types

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+/*
+ * File: 6-size.c
+ * Auth: Peter
+ */
+
+#include <stdio.h>
+
+/**
+ * main - prints the size of data types
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	printf("Size of a char: %d byte(s)\n", sizeof(char));
+	printf("Size of an int: %d byte(s)\n", sizeof(int));
+	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+	printf("size of float: %d bytes(s)\n", sizeof(float));
+	return (0);
+}
