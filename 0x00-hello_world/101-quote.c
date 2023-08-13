2a83d6b67d00c7c930e6e1f2766c7e5630fcf349
Peter Odero <peterodero561@gmail.com>
2023-08-13 13:19:42 +0000

    Quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+/*
+ * File: 101-quote.c
+ * Auth: Peter
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Prints and that piece of art is useful" - Dora Korpar, 2015-10-19
+ * Return: Always 0
+ */
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (0);
+}
