0370145c48487be9018971fdf2cefb2064af20bf
Peter Odero <peterodero561@gmail.com>
2023-08-15 11:08:10 +0000

    printing _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+/*
+ * File: 0-putchar.c
+ * Auth: Peter
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Prints _putchar
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
