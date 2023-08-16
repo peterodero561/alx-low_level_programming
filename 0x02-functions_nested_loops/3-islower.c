2da81eb7355e10172edac2d388654b784ed2d91e
Peter Odero <peterodero561@gmail.com>
2023-08-16 13:05:34 +0000

    Determining whether a given character is lower case or not

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -8,15 +8,16 @@
 /**
  * _islower - determines whether a give character is capital or small
  * letter
+ * @c: cahacter to be checked
  *
  * Return: Always 0
  */
 
 int _islower(int c)
 {
-	int c;
-
 	if (c >= 97 && c <= 122)
+	{
 		return (1);
+	}
 	return (0);
 }
