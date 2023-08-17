80a0705959a734bc9eac8eb2606655b78184e57c
Peter Odero <peterodero561@gmail.com>
2023-08-17 02:21:35 +0000

    printing the 9 times table starting from 0

---
+++ 0x02-functions_nested_loops/9-main.c
@@ -0,0 +1,18 @@
+/*
+ * File: 9-main.h
+ * Auth: Peter
+ */
+
+#include "main.h"
+
+/**
+ * main - prints the 9 times table
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	times_table();
+	return (0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -10,5 +10,6 @@ int print_sign(int n);
 int _abs(int c);
 int print_last_digit(int c);
 void jack_bauer(void);
+void times_table(void);
 
 #endif
