63e3474c6e151b3e27bf405ac1e25634b894662c
Peter Odero <peterodero561@gmail.com>
2023-08-18 17:52:38 +0000

    printing 0 - 14

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,31 @@
+/*
+ * File: 5-more_numbers.c
+ * Auth: Peter
+ */
+
+#include "main.h"
+
+/**
+ * more_numbers - prints 0 - 14
+ *
+ * Return: 0
+ */
+
+void more_numbers(void)
+{
+	int x;
+	int y;
+
+	for (x = 0; x < 10; x++)
+	{
+		for (y = 0; y <= 14; y++)
+		{
+			if (y > 9)
+				_putchar(y / 10 + '0');
+			_putchar(a % 10 + '0');
+		}
+		_putchar(x + '0');
+		a = 0;
+		_putchar('\n');
+	}
+}
