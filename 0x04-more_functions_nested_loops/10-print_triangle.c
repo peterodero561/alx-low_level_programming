4968b90d2e20771c7d82b4f17f52b261502c9190
Peter Odero <peterodero561@gmail.com>
2023-08-19 01:50:56 +0000

    printing a triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,40 @@
+/*
+ * File: 10-print_triangle.c
+ * Auth: Peter
+ */
+
+#include "main.h"
+
+/**
+ * print_triangle - prints a triangle
+ * @size: is the size of the triangle
+ *
+ * Return: Always 0
+ */
+
+void print_triangle(int size)
+{
+	int s;
+	int c;
+	int z;
+
+	if (size > 0)
+	{
+		for (s = 0; s < size; s++)
+		{
+			c = size - s - 1;
+			while (z < size)
+			{
+				if (c > z)
+					_putchar('\n');
+				else
+					_putchar('#');
+				z++;
+			}
+			z = 0;
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n')
+}
