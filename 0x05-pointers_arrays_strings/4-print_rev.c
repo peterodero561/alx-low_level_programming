4400c7fea602d39374c0180a62f6f42c92ffac71
Peter Odero <peterodero561@gmail.com>
2023-08-22 02:59:16 +0000

    printing in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,30 @@
+/*
+ * File: 4-print-rev.c
+ * Auth: Peter
+ */
+
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse
+ * @s: string to be printed in reverse
+ *
+ * Return: Always 0
+ */
+
+void print_rev(char *s)
+{
+	int x;
+	int y;
+
+	x = 0;
+	while (s[x] != '\0')
+	{
+		x++;
+	}
+	for (y = x - 1; y >= 0; y--)
+	{
+		_putchar(s[j]);
+	}
+	_putchar('\n');
+}
