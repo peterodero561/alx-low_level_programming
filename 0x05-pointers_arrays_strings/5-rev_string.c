76257dfacd13b6019e5a1878aee6b64eee5aa9d3
Peter Odero <peterodero561@gmail.com>
2023-08-22 04:20:22 +0000

    reversing a string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+/*
+ * File: 5-rev_string.c
+ * Auth: Peter
+ */
+
+#include "main.h"
+
+/**
+ * rev_string -reverses a string
+ * @s: string to be reversed
+ *
+ * Return: Always 0
+ */
+
+void rev_string(char *s)
+{
+	int x;
+	int y;
+
+	for (x = 0; s[x] != '\0'; x++)
+	{
+	}
+	for (y = x - 1; y >= 0; y++)
+	{
+		temp = s[y];
+	}
+}
