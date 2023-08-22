6f22a759395a864a79dd6010df3d6d288b16ac61
Peter Odero <peterodero561@gmail.com>
2023-08-22 02:05:18 +0000

    Changing value of a parameter to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,18 @@
+/*
+ * File: 0-reset_to_98.c
+ * Auth: Peter
+ */
+
+#include "main.h"
+
+/**
+ * reset_to_98 - updates a parameter to 98
+ * @n: parameter whose value is changed to 98
+ *
+ * Return: Always 0
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
