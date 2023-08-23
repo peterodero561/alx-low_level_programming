ed994450dcda4648d05c5ee13d4a65ee952f14ac
Peter Odero <peterodero561@gmail.com>
2023-08-23 07:46:04 +0000

    printing numbers from 0 - 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+/*
+ * File: 9-print_comb.c
+ * Auth: Peter
+ */
+
+#include <stdio.h>
+
+/**
+ * main - prints all possible combinations of single digit numbers
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i + '0');
+		putchar(',');
+		putchar(32);
+	}
+	putchar('\n');
+	return (0);
+}
