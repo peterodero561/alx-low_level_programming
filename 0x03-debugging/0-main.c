d841f2a70f2ee7c53669bc6872a2b824e3fb9b7e
Peter Odero <peterodero561@gmail.com>
2023-08-23 01:39:13 +0000

    Testing positive or negative function

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,21 @@
+/*
+ * File: 0-main.c
+ * Auth: Peter
+ */
+
+#include "main.h"
+
+/**
+ * main - determines whether a given value is positive or negative
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+	return (0);
+}
