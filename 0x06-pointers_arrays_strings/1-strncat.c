f06ff91570c5727f8da0b6c1dc7a3d2a5f52a83c
Peter Odero <peterodero561@gmail.com>
2023-08-24 13:58:51 +0000

    concatenating two string with agiven number of letters of the other string

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -18,13 +18,14 @@ char *_strncat(char *dest, char *src, int n)
 {
 	int i;
 	int j;
-	int k;
 
 	for (i = 0; dest[i] != '\0'; i++)
 	{}
-	for (j = 0; src[j] == '\0' && j < n; j++)
+	for (j = 0; src[j] != '\0' && n > 0; j++)
 	{
 		dest[i] = src[j];
 		i++;
+		n--;
 	}
+	return (dest);
 }
