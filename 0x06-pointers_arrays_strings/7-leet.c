56f73c02e7cb434630c6248f9303a95c983e9c59
Peter Odero <peterodero561@gmail.com>
2023-08-24 21:21:16 +0000

    encoding a message

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -18,15 +18,15 @@ char *leet(char *c)
 
 	for (i = 0; c[i] != '\0'; i ++)
 	{
-		for (; c[i] == 'a' || c[i] == 'A';)
+		while ( c[i] == 'a' || c[i] == 'A')
 			c[i] = '4';
-		for (; c[i] == 'e' || c[i] == 'E';)
+		while ( c[i] == 'e' || c[i] == 'E')
 			c[i] = '3';
-		for (; c[i] == 'o' || c[i] == 'O';)
+		while ( c[i] == 'o' || c[i] == 'O')
 			c[i] = '0';
-		for (; c[i] == 't' || c[i] == 'T';)
+		while ( c[i] == 't' || c[i] == 'T')
 			c[i] = '7';
-		for(; c[i] == 'l' || c[i] == 'L';)
+		while ( c[i] == 'l' || c[i] == 'L')
 			c[i] = '1';
 	}
 	return (c);
