78f4df8a2e2d3caada81f980f27297d6adc06567
Peter Odero <peterodero561@gmail.com>
2023-08-24 15:15:02 +0000

    Comparing a string

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,25 @@
+/*
+ * File: 3-strcmp
+ * Auth: Peter
+ */
+
+#include "main.h"
+
+/**
+ * _strcmp - compares two strings
+ * @s1: one of the strings
+ * @s2: the other string
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+	int j;
+
+	for (i = 0, j = 0; s1[i] != '\0' || s2[j] != '\0'; i++, j++)
+	{
+		if (s1[i] != s2[j])
+			return (s1[i] - s2[j]);
+	}
+	return (0);
+}
