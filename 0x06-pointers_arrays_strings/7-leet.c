ce8678d2b7c8677d6f2f3a0cfdfd317355bae15e
Peter Odero <peterodero561@gmail.com>
2023-08-25 17:29:18 +0000

    encoding a message

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -21,7 +21,7 @@ char *leet(char *s)
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (j = 0; ltrs[j] = '\0'; j++)
+		for (j = 0; ltrs[j] == '\0'; j++)
 		{
 			if (s[i] == ltrs[j])
 				s[i] = replc[j];
