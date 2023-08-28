72c68786fead3f05d0ba38336daa815dde46dacd
Peter Odero <peterodero561@gmail.com>
2023-08-28 19:55:12 +0000

    Locating a substring

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,33 @@
+/*
+ * File: 5-strstr.c
+ * Auth: Peter
+ */
+
+#include "main.h"
+
+/**
+ * _strstr - locates a substring
+ * @haystack: string being be checked
+ * @needle: string that is being checked in @haystack
+ *
+ * Return: pointer to the beggining of the located substring
+ * or NULL if the string is not found
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i;
+	int j;
+
+	for (i = 0; *(needle + i) != '\0'; i++)
+	{
+		for (j = 0; *(haystack + j) != '\0'; j++)
+		{
+			if (*(needle + i) == *(haystack + j))
+				return (needle + i);
+			else
+				return (0);
+		}
+	}
+	return (needle);
+}
