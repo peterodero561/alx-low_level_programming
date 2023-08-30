febc61e64f11e09fc7d070982b0e9708f7a3173d
Peter Odero <peterodero561@gmail.com>
2023-08-30 16:27:33 +0000

    puts function using recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -14,9 +14,10 @@
 
 void _puts_recursion(char *s)
 {
-	int i;
-
-	for (i = 0; s[i] != '\0'; i++)
-		_putchar(s[i]);
-	_putchar('\n');
+	if (*s != '\0')
+	{	_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+		_putchar('\n');
 }
