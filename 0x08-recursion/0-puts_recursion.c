7f18e64c99fa3b249d6ad00c5e8423d905b3d2ed
Peter Odero <peterodero561@gmail.com>
2023-08-30 11:08:02 +0000

    puts with recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,22 @@
+/*
+ * File: 0-puts_recursion
+ * Auth: Peter
+ */
+
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string followed by a new line
+ * @s: string to be printed
+ *
+ * Return: Nothing
+ */
+
+void _puts_recursion(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+		_putchar(s[i]);
+	_putchar('\n')
+}
