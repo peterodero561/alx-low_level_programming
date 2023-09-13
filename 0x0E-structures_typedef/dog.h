845d9705bc959494dab77e7053477c276655079d
Peter Odero <peterodero561@gmail.com>
2023-09-13 17:14:52 +0000

    Creating a structure to stire info of a dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef dog
+#define dog
+
+/**
+ * struct dog - holds information about a dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ */
+
+struct dog
+{
+	char *name;
+	float *age;
+	char *owner;
+}
+
+#endif
