eed550a171217cb9780cc3d40b966de48cb0c765
Peter Odero <peterodero561@gmail.com>
2023-09-13 12:04:34 +0000

    Using inbuilt macro to print file it was complied from

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,19 @@
+/*
+ * File: 2-main.c
+ * Auth: Peter
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the name of the file it was complied from and a new line
+ *
+ * Return: Always 0
+ */
+
+int main (void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
