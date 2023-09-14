d87545cac008fe9745c3f4a5e7e6a1418ec7bbc7
Peter Odero <peterodero561@gmail.com>
2023-09-14 17:00:22 +0000

    Printing values of a struct using a fuction

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -18,8 +18,7 @@ void print_dog(struct dog *d)
 {
 	if (d == NULL)
 		printf("(nil)");
-	else
-		printf("Name: %s\n", d->name);
-		printf("Age: %f\n", d->age);
-		printf("Owner: %s\n", d->owner);
+	printf("Name: %s\n", d->name);
+	printf("Age: %f\n", d->age);
+	printf("Owner: %s\n", d->owner);
 }
