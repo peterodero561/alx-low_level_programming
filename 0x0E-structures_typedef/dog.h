7c3eaa2399489f069c2f792c5f8628a52a389976
Peter Odero <peterodero561@gmail.com>
2023-09-14 17:12:37 +0000

    Creating a struct using typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -16,5 +16,11 @@ struct dog
 };
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+typedef struct dog_t
+{
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
 
 #endif
