7a5d01ccb40473e985fbc7a110dd60fdd3d4295c
Peter Odero <peterodero561@gmail.com>
2023-09-15 16:52:35 +0000

    Printing using a function pointer

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,19 @@
+/*
+ * File: 0-print_name.c
+ * Auth: Peter
+ */
+
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name
+ * @name: name to be printed
+ * @(*f)(char *): pointer to a function that will print the name
+ *
+ * Return: Nothing
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	(*f)(name);
+}
