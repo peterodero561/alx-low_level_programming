646be510c9bf5cf4b2a356a59fac69c3d1c33919
Peter Odero <peterodero561@gmail.com>
2023-09-22 16:25:51 +0000

    Freeing a linked list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,26 @@
+/*
+ * File: 4-free_list.c
+ * Auth: Peter
+ */
+
+#include "lists.h"
+
+/**
+ * free_list - frees a list_t list
+ * @head: pointer to the list
+ * 
+ * Return: NOthing
+ */
+
+void free_list(list_t *head)
+{
+	list_t *current;
+
+	while (head != NULL)
+	{
+		current = head;
+		head = head->next;
+		free(current->str);
+		free(current);
+	}
+}
