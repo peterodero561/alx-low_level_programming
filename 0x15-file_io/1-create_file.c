a0c5d157e9d27aad812654929b7feb3fb4aa7161
Peter Odero <peterodero561@gmail.com>
2023-10-03 01:49:32 +0000

    Correcting Betty

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,10 +20,10 @@ int create_file(const char *filename, char *text_content)
 	ssize_t bytes_written;
 
 	if (filename == NULL)
-		return -1;
+		return (-1);
 	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	if (fd == -1)
-		return -1;
+		return (-1);
 	if (text_content != NULL)
 	{{
 		content_length = 0;
@@ -34,7 +34,7 @@ int create_file(const char *filename, char *text_content)
 	if (bytes_written == -1)
 	{
 		close(fd);
-		return -1;
+		return (-1);
 	}}
 	close(fd);
 	return (1);
