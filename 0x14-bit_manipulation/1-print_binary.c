4ad4cb301927447968ef7131cc6cd41700871b64
Peter Odero <peterodero561@gmail.com>
2023-10-12 04:22:22 +0000

    corracting betty errors

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -6,28 +6,6 @@
  *
  * Return: Nothing
  */
-/*
-void print_binary(unsigned long int n)
-{
-	int binary_digits[64];
-	int i = 0;
-
-	if (n == 0)
-	{
-		_putchar('0');
-		return;
-	}
-	while (n > 0)
-	{
-		binary_digits[i] = n & 1;
-		n >>= 1;
-		i++;
-	}
-	for (i--; i >= 0; i--)
-	{
-		_putchar(binary_digits[i] + '0');
-	}
-}*/
 
 void print_binary(unsigned long int n)
 {
