b68d0e7a557aca695642196d27f06f93ef7ad881
Peter Odero <peterodero561@gmail.com>
2023-10-13 12:46:36 +0000

    Alterations to function print_all to use case

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -20,28 +20,27 @@ void print_all(const char * const format, ...)
 
 	while (*format_ptr)
 	{
-		if (*format_ptr == 'c')
+		switch (*format_ptr)
 		{
-			c = va_arg(args, int);
-			printf("%c", c);
-		}
-		else if (*format_ptr == 'i')
-		{
-			i = va_arg(args, int);
-			printf("%d", i);
-		}
-		else if (*format_ptr == 'f')
-		{
-			f = va_arg(args, double);
-			printf("%f", f);
-		}
-		else if (*format_ptr == 's')
-		{
-			s = va_arg(args, char *);
-			if (s == NULL)
-				printf("(nil)");
-			else
-				printf("%s", s);
+			case 'c':
+				c = va_arg(args, int);
+				printf("%c", c);
+				break;
+			case 'i':
+				i = va_arg(args, int);
+				printf("%d", i);
+				break;
+			case 'f':
+				f = va_arg(args, double);
+				printf("%f", f);
+				break;
+
+			case 's':
+				s = va_arg(args, char *);
+				if (s == NULL)
+					printf("(nil)");
+				else
+					printf("%s", s);
 		}
 		format_ptr++;
 	}
