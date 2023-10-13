98f749be4e9a3a5db73032c53569e1ab749e2c69
Peter Odero <peterodero561@gmail.com>
2023-10-13 14:11:17 +0000

    A function that prints without being called

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -10,6 +10,5 @@ void print(void) __attribute__((constructor));
 
 void print(void)
 {
-	printf("You're beat! and yet, you must allow,
-			\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
 }
