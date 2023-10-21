5dee153b48531659d0bde6409982d29a6e0881da
Peter Odero <peterodero561@gmail.com>
2023-10-21 17:49:13 +0000

    reading elf header file

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -37,7 +37,8 @@ void display_elf_header(const char *filename)
 		printf("%02x ", (unsigned char)header.e_ident[i]);
 	printf("\n");
 	printf("Class: %d-bit\n", header.e_ident[EI_CLASS] == ELFCLASS32 ? 32 : 64);
-	printf("Data: %s\n", header.e_ident[EI_DATA] == ELFDATA2LSB ? "2's complement, little-endian" : "2's complement, big-endian");
+	printf("Data: %s\n", header.e_ident[EI_DATA] == ELFDATA2LSB ? "2's complement,
+			little-endian" : "2's complement, big-endian");
 	printf("Version: %d\n", header.e_ident[EI_VERSION]);
 	printf("OS/ABI: %d\n", header.e_ident[EI_OSABI]);
 	printf("ABI Version: %d\n", header.e_ident[EI_ABIVERSION]);
