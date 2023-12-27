fd9b0d9c6672a051961f5ab3b601609add5e3f3d
Peter Odero <peterodero561@gmail.com>
2023-12-27 20:09:20 +0000

    Adding node at the begginning of a doubly linked list

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint - adds node in the head of a doubly linked list
+ * @head: points to the head of a list
+ * @n: data to add in the list
+ * Return: address of the new element or Null if failed
+ */
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new;
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return NULL;
+	new->n = n;
+	new->next = *head;
+	new->prev = NULL;
+	if (*head != NULL)
+		(*head)->prev = new;
+	*head = new;
+	return (new);
+}
